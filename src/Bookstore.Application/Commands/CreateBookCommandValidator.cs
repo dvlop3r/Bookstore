09fa45c2bb225cb250a6043bbd1c5255a0184584
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-11 12:02:40 +0300

    Add dateTime extension

--- src/Bookstore.Application/Commands/CreateBookCommandValidator.cs
+++ src/Bookstore.Application/Commands/CreateBookCommandValidator.cs
@@ -1,3 +1,4 @@
+using Bookstore.Application.Extensions;
 using FluentValidation;
 
 namespace Bookstore.Application.Commands;
@@ -7,14 +8,14 @@ public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
     public CreateBookCommandValidator()
     {
         RuleFor(x => x.Request.Title)
-            .NotEmpty().Length(3, 5).WithMessage("Title must be between 3 and 5 characters");
+            .NotEmpty().Length(3, 6).WithMessage("Title must be between 3 and 5 characters");
         RuleFor(x => x.Request.Author)
-            .NotEmpty().Length(3, 5).WithMessage("Author must be between 3 and 5 characters");
+            .NotEmpty().Length(3, 6).WithMessage("Author must be between 3 and 5 characters");
         RuleFor(x => x.Request.Description)
             .NotEmpty().Length(10, 20).WithMessage("Description must be between 10 and 20 characters");
         RuleFor(x => x.Request.CoverImageUrl).NotEmpty().WithMessage("Book cover image url can not be empty");
         RuleFor(x => x.Request.BookUrl).NotEmpty().WithMessage("Book url can not be empty");;
         RuleFor(x => x.Request.PublishDate)
-            .NotEmpty().Must(x => x >= DateTime.Now.Date).WithMessage("Publish date must be greater than now");
+            .NotEmpty().Must(x => x.StartOfDay() >= DateTime.Now.StartOfDay()).WithMessage("Publish date must be greater than now");
     }
 }
---
+++ src/Bookstore.Application/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+namespace Bookstore.Application.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static DateTime StartOfDay(this DateTime date)
+    {
+        return date.Date;
+    }
+    public static DateTime EndOfDay(this DateTime date)
+    {
+        return date.Date.AddDays(1).AddTicks(-1);
+    }
+}
