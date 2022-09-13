5bcb66627d4b25b5f807388c89b5077ac63e19c3
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-13 22:50:32 +0300

    Add enctype multipart-form-data to form submition

--- src/Bookstore.Client/Models/BookViewModel.cs
+++ src/Bookstore.Client/Models/BookViewModel.cs
@@ -1,16 +1,19 @@
 using Bookstore.Client.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bookstore.Client.Models;
 
 public class BookViewModel
 {
     public Guid Id { get; set; }
+    [Range(3, 6, ErrorMessage = "Title must be between 3 and 6 characters")]
     public string? Title { get; set; }
+    [Range(10, 20, ErrorMessage = "Description must be between 10 and 20 characters")]
     public string? Description { get; set; }
     public string? Author { get; set; }
     [BindProperty]
-    public FileUpload? File { get; set; }
+    public FileUpload? Files { get; set; }
 
     [FutureDate]
     public DateTime PublishDate { get; set; }
