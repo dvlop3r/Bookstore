f80e1274c21b09f413a339f29dfd910d2acfbc80
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-13 22:27:38 +0300

    Add data annotation date attribute and FileUpload model

---
+++ src/Bookstore.Client/DataAnnotations/FutureDateAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Client.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            return value is not null && value is DateTime dateTime && dateTime > DateTime.UtcNow
+                ? ValidationResult.Success : new ValidationResult("Date can't be in the past");
+        }
+    }
+}
---
+++ src/Bookstore.Client/Models/FileUpload.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookstore.Client.Models
+{
+    public class FileUpload
+    {
+        public IFormFile? CoverImageFile{ get; set; }
+        public IFormFile? BookFile { get; set; }
+    }
+}
