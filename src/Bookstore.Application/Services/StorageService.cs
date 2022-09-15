b5aa46dab802eaf6e0ccd4683078441bb70661f9
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-16 01:16:08 +0300

    Add get user profile path to storage service

---
+++ src/Bookstore.Application/Services/StorageService.cs
@@ -0,0 +1,22 @@
+﻿using Bookstore.Contracts.Settings;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Services
+{
+    public class StorageService : IStorageService
+    {
+        private readonly IOptions<AppSettings> _options;
+
+        public StorageService(IOptions<AppSettings> options)
+        {
+            _options = options;
+        }
+
+        public Task GetUserProfilePath() => await Task.FromResult(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), _options.Value.Storage));
+    }
+}
