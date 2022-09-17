d18de0c9ff52bfd74d944ce8eba087ba87de6e40
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-17 19:45:25 +0300

    Add book and cover image download actions

--- src/Bookstore.Client/Services/FileStorageService.cs
+++ src/Bookstore.Client/Services/FileStorageService.cs
@@ -38,10 +38,10 @@ namespace Bookstore.Client.Services
                     await model.Files.CoverImageFile.CopyToAsync(fileStream);
             }
         }
-        public async Task<(byte[],string,string)> DownloadFileAsync(Guid id, string path)
+        public async Task<(byte[],string,string)> DownloadFileAsync(string filePath)
         {
             await Task.CompletedTask;
-            var filePath = Path.Combine(path, id.ToString(), path);
+            var exists = System.IO.File.Exists(filePath);
             byte[] bytes = System.IO.File.ReadAllBytes(filePath);
             var fileName = Path.GetFileName(filePath);
             var extension = Path.GetExtension(filePath);
