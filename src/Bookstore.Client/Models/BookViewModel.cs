e8ca7ac940e87d5dfdd22217c8748be914d596ae
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-17 16:16:53 +0300

    Change errors css on create and update

--- src/Bookstore.Client/Models/BookViewModel.cs
+++ src/Bookstore.Client/Models/BookViewModel.cs
@@ -9,7 +9,7 @@ public class BookViewModel
     // All validations can be removed to let backend do the evaluation
 
     public Guid Id { get; set; }
-    //[Required]
+    [Required]
     [TitleLength]
     public string? Title { get; set; }
     //[StringLength(20, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 20 characters")]
