5e6e28298649459c40e00b8bc4d7576c43cc8062
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-19 22:38:21 +0300

    Add api versioning configuration

--- src/Bookstore.Api/DependencyInjection.cs
+++ src/Bookstore.Api/DependencyInjection.cs
@@ -1,5 +1,6 @@
-using Bookstore.Api.Filters;
 using Bookstore.Contracts.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace Bookstore.Api;
 public static class DependencyInjection
@@ -12,11 +13,39 @@ public static class DependencyInjection
         // services.AddControllers(options => options.Filters.Add(new GlobalExceptionFilter()));
         services.AddControllers();
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.ConfigureApiVersioning();
 
         // adds options pattern
         services.Configure<AppSettings>(configuration);
         
+        return services;
+    }
+
+    public static IServiceCollection ConfigureApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(setup =>
+            {
+                setup.DefaultApiVersion = new ApiVersion(1, 0);
+                setup.AssumeDefaultVersionWhenUnspecified = true;
+                setup.ReportApiVersions = true;
+            });
+
+        services.AddVersionedApiExplorer(setup =>
+        {
+            setup.GroupNameFormat = "'v'VVV";
+            setup.SubstituteApiVersionInUrl = true;
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            //Hardcoding api version documentation is not a good practice because
+            //of multiple versions, we use ConfigureSwaggerOptions class instead"
+            https://localhost:5001/swagger/index.html?urls.primaryName=V1
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookstoreWebAPI", Version = "v1" });
+            c.SwaggerDoc("v2", new OpenApiInfo { Title = "BookstoreWebAPI", Version = "v2" });
+        });
+        //services.ConfigureOptions<ConfigureSwaggerOptions>();
+
         return services;
     }
 }
